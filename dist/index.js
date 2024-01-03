042ee82176647a99ed1b45a37970315b07a2b128
Daniel Patton <65815343+dpatton1992@users.noreply.github.com>
2024-01-03 14:02:53 -0500

    chore: repackaging

--- dist/index.js
+++ dist/index.js
@@ -60,15 +60,13 @@ const restClient = axios_1.default.create({
     }
 });
 const postmanWorkspaceId = core.getInput('postmanWorkspaceId');
-// const specPath = core.getInput('specPath')
-const specPath = './dist/postman.json';
-if (specPath)
-    core.info(`Using 'specPath' (${specPath}) input to load Postman Collection`);
+const specPath = core.getInput('specPath');
 const addLocalSpecFile = (file) => __awaiter(void 0, void 0, void 0, function* () {
     var _a;
     // Read the file content in memory and convert to JSON
     try {
         const jsonContent = JSON.parse((yield fs_1.promises.readFile(file)).toString());
+        core.info(jsonContent);
         // Check if the JSON file is a "valid" Postman v2.1 Collection, when true store in array
         if (((_a = jsonContent === null || jsonContent === void 0 ? void 0 : jsonContent.info) === null || _a === void 0 ? void 0 : _a.schema) ===
             `https://schema.getpostman.com/json/collection/v2.1.0/collection.json`) {
